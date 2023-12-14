1107aa19138cddf8132ed54cd0efd7359bbab761
kirigayaUA <kirigayaua@gmail.com>
2023-12-15 01:23:57 +0200

    New file

---
+++ my_project_6.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <iomanip> 
+#include <windows.h>
+#include <math.h>
+using namespace std;
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	double x, y, a, b, c;
+	cout << "Введіть значення a: ";
+	cin >> a;
+	cout << "Введіть значення b: ";
+	cin >> b;
+	cout << "Введіть значення c: ";
+	cin >> c;
+	x = a;
+	do {
+		y = pow(x, 1 / 3.0) + pow(x, 3) - 3;
+		cout << "x=" << x << "\t" << "y=" << y << endl;
+		x += c;
+	} while (x <= b);
+}
+
+
+
