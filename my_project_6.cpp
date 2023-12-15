0e8b55ed35dfaced41760c27ab2d0b1eed891d39
kirigayaUA <kirigayaua@gmail.com>
2023-12-15 02:08:12 +0200

    add cout

--- my_project_6.cpp
+++ my_project_6.cpp
@@ -1,22 +1,10 @@
 ﻿#include <iostream>
-#include <windows.h>
-#include <math.h>
+
 using namespace std;
 int main()
 {
-	int x, y, a, b, c;
-	cout << "input a: ";
-	cin >> a;
-	cout << "input b: ";
-	cin >> b;
-	cout << "input c: ";
-	cin >> c;
-	x = a;
-	do {
-		y = pow(x, 1 / 3.0) + pow(x, 3) - 3;
-		cout << "x=" << x << "\t" << "y=" << y << endl;
-		x += c;
-	} while (x <b);
+	cout << "Hello world";
+	return 0;
 }
 
 
